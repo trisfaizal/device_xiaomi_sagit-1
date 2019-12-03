e473285bb1b128248eaaef1419f51070c3d74a7d
0ranko0P <ranko0p@outlook.com>
2019-12-03 23:38:19 +0800

    Revert "sagit: Add MIUICamera"
    Use Google Camera in GApps build
    
    This reverts commit f37edbf51d484efdcd800eed3a0e3e913482d969.

--- device.mk
+++ device.mk
@@ -28,9 +28,6 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
-# MiuiCamera
-$(call inherit-product, vendor/apps/MiuiCamera/config.mk)
-
 # Wallpapers
 PRODUCT_PACKAGES += \
     WallpapersBReel2019
