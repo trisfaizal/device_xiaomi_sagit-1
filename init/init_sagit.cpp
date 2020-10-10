bed5710923898b752d38d6901e36096a45531962
Arian <arian.kulmer@web.de>
2020-10-10 16:46:31 +0200

    sagit: init: Cleanup unused includes
    Change-Id: Ib713665e84bdfb3e41e6d54618308b32cfba8f1b
    Signed-off-by: 0ranko0P <ranko0p@outlook.com>

--- init/init_sagit.cpp
+++ init/init_sagit.cpp
@@ -32,10 +32,6 @@
 #include <sys/_system_properties.h>
 
 #include <android-base/properties.h>
-#include "property_service.h"
-#include "vendor_init.h"
-
-using android::init::property_set;
 
 void property_override(char const prop[], char const value[])
 {
